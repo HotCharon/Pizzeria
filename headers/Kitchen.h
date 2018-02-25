b2ebb471b369cbbb7cc54cff116558e87b9fcbb7
Yuri <hamta@yandex.ru>
2018-02-25 19:31:52 +0300

    Kitchen is ready!

--- headers/Kitchen.h
+++ headers/Kitchen.h
@@ -6,11 +6,14 @@
 
 class Kitchen {
 public:
-	Set* CreateSet(SetChef& cc) {
+	Set* CreateSet(SetChef& cc, int countOfPizzas, int countOfDrinks) {
 		Set* set = new Set;
-
-		set->AddPizza(cc.CreatePizza());
-		set->AddDrink(cc.CreateDrink());
+		
+		for (size_t i = 0; i < countOfPizzas; i++)
+			set->AddPizza(cc.CreatePizza());
+		
+		for (size_t i = 0; i < countOfDrinks; i++)
+			set->AddDrink(cc.CreateDrink());
 
 		return set;
 	}
